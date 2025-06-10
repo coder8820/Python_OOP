dbc712a03814059c828a4d4225ccd956fd202d48
coder8820 <kumail112113@gmail.com>
2025-06-10 06:54:33 -0700

    learned spreadOperator

---
+++ PythonFundamentals/spreadOperator.py
@@ -0,0 +1,7 @@
+arr1 = ['ali', 'veli', 'deli']
+arr2 = ['ahmet', 'mehmet', 'hasan']
+# Spread operator
+arr3 =[*arr1, *arr2]
+
+arr4 = [*arr1, *arr2, 'ayse','fatma', 'zeynep', 'esra', 'seda', 'selin', 'seda', 'selin']
+print(arr4)
